36281b8f3cd32262acfed9962a3fcba13e1d5892
Musa <smabumusa98gmail.com>
2021-09-19 20:30:51 +0600

    success

---
+++ src/components/Nav.js
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+import classes from "../styles/Nav.module.css";
+import Account from "./Account";
+
+export default function Nav() {
+  return (
+    <nav className={classes.nav}>
+      <ul>
+        <li>
+          <Link to="/" className={classes.brand}>
+        
+            <h3>QUIZ</h3>
+          </Link>
+        </li>
+      </ul>
+      <Account />
+    </nav>
+  );
+}
